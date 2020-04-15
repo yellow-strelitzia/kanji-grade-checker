879673175bbaa43e7e1caf2425ef714fb9ba9e35
yellow-strelitzia <kunigogo.dev.public@gmail.com>
2020-04-15 00:45:25 +0000

    remove await keyword

--- api/ocr.js
+++ api/ocr.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   const url = 'http://yellow-strelitzia-ocr-server1.herokuapp.com/';
   
   if ( Object.keys(req.query) > 0 ){
@@ -10,22 +10,26 @@ module.exports = async (req, res) => {
   }
 
   if ( parameters.type == 'echo' ) {
-    let axios_result = await axios.get(url + 'echo');
-    res.status(200).send(axios_result.data);
-    console.log('echo called');
+    axios.get( url + 'echo' )
+         .then( axios_result => {
+           res.status(200).send(axios_result.data);
+           console.log('echo called');           
+    });
   } else if ( parameters.type == 'recognize' ) {
-    let axios_result = await axios.post(url + 'recognize', {
-      data: parameters.data,
-      direction: parameters.direction
+    axios.post( url + 'recognize', {
+            data: parameters.data,
+            direction: parameters.direction } )
+         .then( axios_result => {
+           res.status(200).json(axios_result.data);
+           console.log('recognize result > ' + axios_result.data.status);
     });
-    console.log('recognize result > ' + axios_result.data.status);
-    res.status(200).json(axios_result.data);
   } else if ( parameters.type == 'result') {
-    let axios_result = await axios.get(url + 'result', {
-      params: {
-        data: parameters.requestid
-      }
+    axios.get(url + 'result', {
+            params: {
+            data: parameters.requestid } )
+         .then( axios_result => {
+           res.status(200).json(axios_result.data);
+           console.log('result extracted > ' + parameters.requestid );
     });
-    res.status(200).json(axios_result.data);  
   }  
 }
